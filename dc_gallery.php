5cae254af99e767059d4095e821dce5f6bef08d6
Design Cart <46775419+DesignCart@users.noreply.github.com>
2026-03-25 05:45:46 +0100

    Add files via upload

--- dc_gallery.php
+++ dc_gallery.php
@@ -1,8 +1,26 @@
 <?php
-/**
- * @package     Joomla.Plugin
- * @subpackage  Content.dc_gallery
- */
+    /**
+     * @package     DC Gallery
+     * @subpackage  Content Plugin
+     * @author      Design Cart
+     * @copyright   Copyright (C) 2025 Design Cart. All rights reserved.
+     * @license     GNU General Public License version 3 or later; see LICENSE.txt
+     *
+     * This file is part of DC Gallery.
+     *
+     * DC Gallery is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * DC Gallery is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with DC Gallery. If not, see <https://www.gnu.org/licenses/>.
+    */
 
 defined('_JEXEC') or die;
 
